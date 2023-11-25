050bbf7eb0a850792d8d8d4f6fcacb65d59e274b
root <root@Servidor.localdomain>
2023-11-25 11:53:47 -0300

    Rotas de criação elistagem - Métodos HTTP

--- src/server.js
+++ src/server.js
@@ -1,11 +1,42 @@
 //importaçao de modulos internos por padrão coloca-se o prefixo node:
 
 import http  from 'node:http'
-
+/**
+ * Teremos as seguintes rotas
+ * Criar usuario
+ * listagem de usuario
+ * ediçao de usuario
+ * remoção de usuario
+ * entre outras dentro da demanda da aplicação
+ * 
+ * Principais metodos HTTP
+ * GET, POST, PUT, PATCH, DELETE
+ * 
+ * GET => Busca um recurso do back-end
+ * POST => criar um recurso no back-end
+ * PUT => Atualizar um recurso no back-end
+ * PATCH => Atualiar uma informação especifica de um recurso no back-end
+ * DELETE => Deletar um recurso do back-end
+ * 
+ * Iremos diferenciar qual metodo usar na requesiçao utilizando a soma da URL + METODO
+ * 
+ * GET/users => Estamos buscando usuários no back-end
+ * POST/users => Estamos criando um usuário no back-end
+ */
 
 
 
 const server =http.createServer((req, res)=>{
-  return res.end('Hello World')
+const {method, url} = req
+
+ if(method === 'GET' && url ==='/users'){
+  return res.end('Listagem de usuários')
+ }
+
+ if(method ==='POST' && url === '/users'){
+  return res.end('Criação de usuário')
+ }
+
+  return res.end('Hello World1')
 })
 server.listen(3333)
