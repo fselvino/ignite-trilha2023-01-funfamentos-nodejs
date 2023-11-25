1230f2715921649c459fd0e22d2e65f54a3097a3
root <root@Servidor.localdomain>
2023-11-25 15:27:51 -0300

    Stream de escrita e transformação

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -8,7 +8,7 @@
 //process.stdin
  // .pipe(process.stdout)//pipe metodo que encaminha a informaçao recebida
 
- import {Readable} from 'node:stream'
+ import {Readable, Writable, Transform} from 'node:stream'
 
  class OneToHundredStream extends Readable {
   index = 1
@@ -27,6 +27,25 @@
     }, 1000);
     
   }
+
+
  }
 
- new OneToHundredStream().pipe(process.stdout)
+ class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback){
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+ }
+
+ class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback){
+    const transformed = Number(chunk.toString() * -1)
+    callback(null, Buffer.from(String(transformed)))
+  }
+ }
+
+
+ new OneToHundredStream()
+ .pipe(new InverseNumberStream())
+ .pipe(new MultiplyByTenStream())
